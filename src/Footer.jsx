8660d16520c933b28e787a17d4914f43bd56610c
Harsha1510 <dorapalliharsha@gmail.com>
2025-08-05 23:57:15 +0530

    Resolved Merges

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
@@ -15,21 +14,3 @@ function Footer() {
 }
 
 export default Footer;
-=======
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Footer.css";
-
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2024 Grocery App. All rights reserved.</p>
-      <div className="footer-links">
-        <Link to="/terms">Terms and Conditions</Link> 
-      </div>
-    </footer>
-  );
-}
-
-export default Footer;
->>>>>>> 6c03f43 (Created a fake json-server for API)
